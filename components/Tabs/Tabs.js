f93853a7cb64759a01ee000f996a223d27d77a59
Mada Gower <gmgower@gmail.com>
2019-06-13 21:23:14 -0500

    Use forEach method remove 'tabs-link-selected' class from all of the links.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -32,10 +32,7 @@ const links = document.querySelectorAll('.tabs-link')
 
     // 10 Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
-Array.from(links).forEach(link => {
-  link.classList.remove('tabs-link-selected')
-})
-
+Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'))
 
     // Add a class named "tabs-link-selected" to this link
     // this.element;
