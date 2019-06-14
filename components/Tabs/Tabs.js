0f434bc281c363da38132ecbcc79c9ee1f703342
Mada Gower <gmgower@gmail.com>
2019-06-13 21:36:31 -0500

    Add click event listener TabLink instance, call select method on click.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -23,7 +23,10 @@ class TabLink {
     this.tabItem = new TabItem(this.itemElement);
 
 
-    // Add a click event listener on this instance, calling the select method on click
+    // 13 Add a click event listener on this instance, calling the select method on click
+this.tabElementProperty.addEventListener('click', () => {
+  this.select()
+})
 
   };
 
@@ -31,7 +34,7 @@ class TabLink {
     //  9 Get all of the elements with the tabs-link class
     // const links;
 const links = document.querySelectorAll('.tabs-link')
-
+// console.log('tab click')
 
     // 10 Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
