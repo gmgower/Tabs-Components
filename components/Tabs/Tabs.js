129580030b5725c464269fbcc3e1a8e02c052fc0
Mada Gower <gmgower@gmail.com>
2019-06-13 21:25:36 -0500

    Add 'tabs-link-selected' this.tabElementProperty.

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -17,9 +17,11 @@ class TabLink {
     this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`)
     // console.log(this.itemElement)
 
+
     //  7 Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
     this.tabItem = new TabItem(this.itemElement);
+
 
     // Add a click event listener on this instance, calling the select method on click
 
@@ -30,12 +32,16 @@ class TabLink {
     // const links;
 const links = document.querySelectorAll('.tabs-link')
 
+
     // 10 Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
 Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'))
 
-    // Add a class named "tabs-link-selected" to this link
+
+    // 11 Add a class named "tabs-link-selected" to this link
     // this.element;
+this.tabElementProperty.classList.add('tabs-link-selected');
+
 
     // Call the select method on the item associated with this link
 
